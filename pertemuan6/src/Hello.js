b1d65d4df614e2734bc16cad9e11bceff4531223
Heru Joko Utomo <herujokoutomo@gmail.com>
2017-10-02 22:40:46 +0800

    pertemuan 6

---
+++ pertemuan6/src/Hello.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Hello extends Component {
+
+    render(){
+        return(
+            <p>Hello</p>
+        )
+    }
+
+}
+
+export default Hello;
